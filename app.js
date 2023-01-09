0d1cdc86e179e6547e410c00d8d20b2ea4cad392
hosecloud <4osecloud@gmail.com>
2023-01-09 17:51:16 +0900

    Add login form

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const grettingMsg = document.querySelector("#greeting-msg");
+
+const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    
+    showGreetingMsg();
+    printGreetingMsg();
+    hideLoginForm();
+}
+function showGreetingMsg() {
+    grettingMsg.classList.remove(HIDDEN_CLASSNAME);
+}
+function printGreetingMsg() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    grettingMsg.innerHTML = `Hello ${username}`;
+}
+function hideLoginForm() {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
