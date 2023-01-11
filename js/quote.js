914884e8e7c27fa99bd021636baceec1c369262a
hosecloud <4osecloud@gmail.com>
2023-01-11 15:19:29 +0900

    Add quote in index.html

---
+++ js/quote.js
@@ -0,0 +1,30 @@
+const qutoes = [
+    {
+        quote: "I think everyday about wanting to eat curry every morning.",
+        author: "hose"
+    },
+    {
+        quote: "I do shit after out my bed.",
+        author: "4ose"
+    },
+    {
+        quote: "I exercise everyday... That is my hope.",
+        author: "HOSE"
+    },
+    {
+        quote: "I want to be smart.",
+        author: "dummy hose"
+    },
+    {
+        quote: "Everyday i'm hungry.",
+        author: "hose"
+    },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const num = Math.floor(Math.random() * qutoes.length);
+
+quote.innerHTML = qutoes[num].quote;
+author.innerHTML = qutoes[num].author;
